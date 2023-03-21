811c0fca88979851ab6a363f87e2650af411368d
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-03-21 19:59:48 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,63 @@
 
-import React from "react";
-import './../styles/App.css';
+
+import './styles.css';
+import React, { useState } from 'react';
 
 const App = () => {
+        
+
+const products = [
+  { id: 1, name: 'Samsung Galaxy Fold 4' },
+  { id: 2, name: 'Iphone 14Pro'},
+  { id: 3, name: 'Pixel5' },
+  { id: 4, name: 'Mi note 6'},
+  { id: 5, name: 'FireStick'},
+  {id :6, name:'OnePlus Nord'}
+];
+
+  const [cart, setCart] = useState([]);
+  
+  const handleAddToCart = (product) => {
+    setCart([...cart, product]);
+  };
+  
+  const handleRemoveFromCart = (product) => {
+    const updatedCart = cart.filter((item) => item.id !== product.id);
+    setCart(updatedCart);
+  };
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Products</h2>
+      <div className="container ">
+        {products.map((product) => (
+          <div key={product.id} className="item">
+            <h3 >{product.name}</h3>
+            
+            <button className="displaybtn" onClick={() => handleAddToCart(product)}>Add to cart</button>
+          </div>
+        ))}
+      </div>
+      
+      <h2>Cart</h2>
+      <div className="cart">
+        {cart.length === 0 ? (
+          <p>No items in cart</p>
+        ) : (
+          cart.map((item) => (
+            <div key={item.id} className="item">
+              <h3 className="p-2">{item.name}</h3>
+              
+              <button  onClick={() => handleRemoveFromCart(item)}>Remove from cart</button>
+            </div>
+          ))
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+
+   
 
 export default App
